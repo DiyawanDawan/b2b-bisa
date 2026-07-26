cc40fcc09086d95ec7ac949a7218bb5945007bd9
DiyawanDawan <diyawan0@gmail.com>
2026-07-26 15:43:54 +0800

    upate

--- prisma/migrations/20260726185000_company_profile/migration.sql
+++ prisma/migrations/20260726185000_company_profile/migration.sql
@@ -8,4 +8,7 @@ CREATE TABLE IF NOT EXISTS `CompanyProfile` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `CompanyProfile` ADD CONSTRAINT `CompanyProfile_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `Address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `CompanyProfile` 
+ADD CONSTRAINT `CompanyProfile_address_id_fkey` 
+FOREIGN KEY (`address_id`) REFERENCES `addresses`(`id`) 
+ON DELETE RESTRICT ON UPDATE CASCADE;
